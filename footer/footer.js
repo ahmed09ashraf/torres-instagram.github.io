4cd2a1c40274f04aea4a39d61796bf3332a01ba8
ahmed09ashraf <ahmedashraf097@gmail.com>
2023-02-09 04:45:55 +0200

    modify

---
+++ footer/footer.js
@@ -0,0 +1,23 @@
+let instaFooter = `
+<div class="footer">
+<ul>
+  <li><a href="">Meta</a></li>
+  <li><a href="">About</a></li>
+  <li><a href="">Blog</a></li>
+  <li><a href="">Jobs</a></li>
+  <li><a href="">HELP</a></li>
+  <li><a href="">API</a></li>
+  <li><a href="">PRIVACY</a></li>
+  <li><a href="">Terms</a></li>
+  <li><a href="">Top Accounts</a></li>
+  <li><a href="">Locations</a></li>
+  <li><a href="">Instagram Lite</a></li>
+  <li><a href="">Contact Uploading & Non-Users</a></li>
+  <li><a href="">LANGUAGE</a></li>
+  </ul>
+<p>© 2023 Instagram from Meta</p>
+</div>
+
+`
+
+document.querySelector('footer').innerHTML = instaFooter;
